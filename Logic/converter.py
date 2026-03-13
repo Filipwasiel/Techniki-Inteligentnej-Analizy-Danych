eb2e0995027e0d98ddc507815424f9c24a850a5e
Filip Wasiel <filipwasiel@gmail.com>
2026-03-13 12:08:50 +0100

    Added more options for user

--- Logic/converter.py
+++ Logic/converter.py
@@ -35,56 +35,62 @@ class DocumentConverter:
             df = df.drop(columns=["L.p."])
         df = df.fillna("")
         doc = Document()
-        try:
-            margin = Cm(float(config["margin"]))
-        except ValueError:
-            margin = Cm(2.0)
 
-        alignments_map = {
-            "Do lewej": WD_ALIGN_PARAGRAPH.LEFT,
-            "Do prawej": WD_ALIGN_PARAGRAPH.RIGHT,
-            "Wyjustuj": WD_ALIGN_PARAGRAPH.JUSTIFY,
-            "Do środka": WD_ALIGN_PARAGRAPH.CENTER,
-        }
+        font_name = config.get("font_family", "Arial")
+        font_size = int(config.get("font_size", 12))
+        line_spacing = float(config.get("line_spacing", 1.15))
+        margin = Cm(float(str(config.get("margin", 2.0)).replace(',', '.')))
 
         for section in doc.sections:
-            section.top_margin = margin
-            section.left_margin = margin
-            section.bottom_margin = margin
-            section.right_margin = margin
+            section.top_margin = section.bottom_margin = margin
+            section.left_margin = section.right_margin = margin
             if config.get("page_numbers", True):
                 footer = section.footer
                 footer_p = footer.paragraphs[0] if footer.paragraphs else footer.add_paragraph()
                 footer_p.alignment = WD_ALIGN_PARAGRAPH.CENTER
-                footer_p.text = ""
                 footer_p.add_run("Strona ")
                 DocumentConverter.add_page_number(footer_p.add_run())
 
         if title:
             heading = doc.add_heading(title, 0)
+            # heading.name = font_name
             heading.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
-        for index, row in df.iterrows():
-            separator = doc.add_paragraph()
-            separator.paragraph_format.keep_with_next = True
-            separator.alignment = WD_ALIGN_PARAGRAPH.CENTER
-            run_sep = separator.add_run(f"Rekord {index + 1}")
-            run_sep.bold = True
-            run_sep.font.size = Pt(int(config["font_size"]) + 2)
-            items = list(row.items())
-            for i, (name, val) in enumerate(items):
-                p = doc.add_paragraph()
+        if config.get("format_type") == "Table":
+            table = doc.add_table(rows=1, cols=len(df.columns))
+            table.style = 'Light Shading'
+            hdr_cells = table.rows[0].cells
+            for i, column in enumerate(df.columns):
+                hdr_cells[i].text = str(column)
 
-                if i < len(items) - 1:
-                    p.paragraph_format.keep_with_next = True
-                p.paragraph_format.keep_together = True
-                run_col = p.add_run(f"{DocumentConverter.clean_text(name)}: ")
-                run_col.bold = True
-                run_col.font.size = Pt(int(config["font_size"]))
-                run_val = p.add_run(str(DocumentConverter.clean_text(val)))
-                run_val.font.size = Pt(int(config["font_size"]))
+            for _, row in df.iterrows():
+                row_cells = table.add_row().cells
+                for i, val in enumerate(row):
+                    row_cells[i].text = DocumentConverter.clean_text(val)
+        else:
+            for index, row in df.iterrows():
+                separator = doc.add_paragraph()
+                separator.paragraph_format.keep_with_next = True
+                separator.alignment = WD_ALIGN_PARAGRAPH.CENTER
+                run_sep = separator.add_run(f"Rekord {index + 1}")
+                run_sep.bold = True
+                run_sep.name = font_name
+                run_sep.font.size = Pt(font_size + 2)
+                items = list(row.items())
+                for i, (name, val) in enumerate(items):
+                    p = doc.add_paragraph()
+                    p.paragraph_format.line_spacing = line_spacing
+                    if i < len(items) - 1:
+                        p.paragraph_format.keep_with_next = True
+                    p.paragraph_format.keep_together = True
+                    run_col = p.add_run(f"{DocumentConverter.clean_text(name)}: ")
+                    run_col.bold = True
+                    run_col.font.name = font_name
+                    run_col.font.size = Pt(font_size)
+                    run_val = p.add_run(str(DocumentConverter.clean_text(val)))
+                    run_val.font.name = font_name
+                    run_val.font.size = Pt(font_size)
 
-        # doc.add_page_break()
         doc.save(output_path)
 
     @staticmethod
--- UI/main_window.py
+++ UI/main_window.py
@@ -3,15 +3,14 @@ from Logic.converter import DocumentConverter
 from Utils.settings_loader import load_settings, save_settings
 from PySide6.QtWidgets import (QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton,
                                QLineEdit, QGroupBox, QFormLayout, QMessageBox, QHBoxLayout, QFileDialog, QCheckBox,
-                               QRadioButton, QButtonGroup)
+                               QRadioButton, QButtonGroup, QComboBox, QDoubleSpinBox)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Converter xlsx to docs/pdf")
-        self.resize(800, 600)
-
+        self.resize(500, 650)
         self.filepath = None
         self.settings = load_settings()
 
@@ -26,7 +25,6 @@ class MainWindow(QMainWindow):
         self.btn_browse = QPushButton("Browse")
         self.btn_browse.clicked.connect(self.browse_file)
         self.layout.addWidget(self.btn_browse)
-
         self.file_label = QLabel("Brak wybranego pliku")
         self.file_label.setStyleSheet("color: red;")
         self.layout.addWidget(self.file_label)
@@ -35,16 +33,36 @@ class MainWindow(QMainWindow):
         self.title_entry = QLineEdit()
         self.layout.addWidget(self.title_entry)
 
-        settings_group = QGroupBox("3. User Settings")
+        settings_group = QGroupBox("3. Document Settings")
         form_layout = QFormLayout()
-
+        # Format wyświetlania danych
+        self.combo_format = QComboBox()
+        self.combo_format.addItems(["List", "Table"])
+        # Ustawienia związane z czcionką
         self.font_size_input = QLineEdit(self.settings.get("font_size", "11"))
-        self.margin_input = QLineEdit(self.settings.get("margin", "2.0"))
-        self.page_num_check = QCheckBox("Dodaj numery stron na dole")
+        self.combo_font_name = QComboBox()
+        self.combo_font_name.addItems(["Arial", "Calibri", "Times New Roman", "Verdana"])
+        self.combo_font_name.setCurrentText(self.settings.get("font_name", "Arial"))
+        #Ustawienia związane z marginesami
+        self.margin_val = QDoubleSpinBox()
+        self.margin_val.setRange(0.5, 5.0)
+        self.margin_val.setValue(float(self.settings.get("margin_val", 2.0)))
+        #Ustawienia związane linespacing
+        self.line_spacing = QDoubleSpinBox()
+        self.line_spacing.setRange(1.0, 3.0)
+        self.line_spacing.setSingleStep(0.1)
+        self.line_spacing.setValue(float(self.settings.get("line_spacing", 1.15)))
+        #Ustawienia związane z numerowaniem stron
+        self.page_num_check = QCheckBox("Numerowanie stron")
         self.page_num_check.setChecked(self.settings.get("page_numbers", True))
+
+        #Formularz
+        form_layout.addRow("Display format: ", self.combo_format)
+        form_layout.addRow("Font family: ", self.combo_font_name)
         form_layout.addRow("Font size: ", self.font_size_input)
-        form_layout.addRow("Margin: ", self.margin_input)
-        form_layout.addRow("", self.page_num_check)
+        form_layout.addRow("Line spacing: ", self.line_spacing)
+        form_layout.addRow("Margins (cm): ", self.margin_val)
+        form_layout.addRow(self.page_num_check)
 
         btn_save = QPushButton("Save settings")
         btn_save.clicked.connect(self.save_user_settings)
@@ -53,21 +71,18 @@ class MainWindow(QMainWindow):
         settings_group.setLayout(form_layout)
         self.layout.addWidget(settings_group)
 
-        self.layout.addWidget(QLabel("<b>4. Generuj:</b>"))
+        # self.layout.addWidget(QLabel("<b>4. Generuj:</b>"))
+        #Przyciski generowania
         btn_layout = QHBoxLayout()
-
-        self.btn_docx = QPushButton("Convert to docx")
+        self.btn_docx = QPushButton("Generate docx")
         self.btn_docx.setStyleSheet("background-color: #add8e6;")
         self.btn_docx.clicked.connect(lambda: self.process_conversion("docx"))
-
-        self.btn_pdf = QPushButton("Konwertuj do PDF")
+        self.btn_pdf = QPushButton("Generate PDF")
         self.btn_pdf.setStyleSheet("background-color: #90ee90;")
         self.btn_pdf.clicked.connect(lambda: self.process_conversion("pdf"))
-
         btn_layout.addWidget(self.btn_docx)
         btn_layout.addWidget(self.btn_pdf)
         self.layout.addLayout(btn_layout)
-
 
     def browse_file(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Open Excel file", "Utils", "Excel Files (*.xlsx)")
@@ -78,29 +93,32 @@ class MainWindow(QMainWindow):
 
     def save_user_settings(self):
         new_settings = {
+            "font_family": self.combo_font_name.currentText(),
             "font_size": self.font_size_input.text(),
-            "margin": self.margin_input.text(),
+            "line_spacing": str(self.line_spacing.value()),
+            "margin": str(self.margin_val.text()),
         }
         save_settings(new_settings)
-        QMessageBox.information(self, 'Zapisano', 'Ustawienia zostały zapisane!')
+        QMessageBox.information(self, 'Saved', 'Settings were successfully saved!')
 
     def process_conversion(self, format_type):
         if not self.filepath:
-            QMessageBox.critical(self, "Błąd", "Najpierw wybierz plik Excel!")
+            QMessageBox.critical(self, "Error", "Choose Excel File!")
             return
 
         base_name = os.path.splitext(self.filepath)[0]
         docx_path = f"{base_name}.docx"
         title = self.title_entry.text()
         config = {
+            "format_type": self.combo_format.currentText(),
+            "font_family": self.combo_font_name.currentText(),
             "font_size": self.font_size_input.text(),
-            "margin": self.margin_input.text(),
+            "line_spacing": self.line_spacing.value(),
+            "margin": self.margin_val.text(),
             "page_numbers": self.page_num_check.isChecked(),
         }
-
         try:
             DocumentConverter.generate_docx(self.filepath, docx_path, title, config)
-
             if format_type == "docx":
                 QMessageBox.information(self, "Sukces", f"Zapisano plik:\n{docx_path}")
             elif format_type == "pdf":
