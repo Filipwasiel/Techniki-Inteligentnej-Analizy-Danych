5bfe41281aaacf7c2fe113b5978143ea1809eb50
251546 Szymon Kaźmierczak <szymon.kazmierczak60@gmail.com>
2026-05-18 23:26:07 +0200

    modyfikacje dotyczace aktualizacji predkosci i mutacji - zeby nie ograniczala algorytm predkoscia czastek i zwiekszenie przestrzeni przeszukiwania w koncowych fazach iteracyjnych, inny sposób mutacji, mean_error nieduzo wiekszy, najlepszy i najgorszy wynik mniejszy niz w surowym algorytmie

--- AlgorythmGGL_PSOD/src/GGL_PSOD.py
+++ AlgorythmGGL_PSOD/src/GGL_PSOD.py
@@ -115,4 +115,83 @@ class GGL_PSOD_Raw(GGL_PSOD):
         return self.gbest, self.gbest_fit
 
 class GGL_PSOD_Modified(GGL_PSOD):
-    pass
+    def run(self):
+        for iter in range(self.max_iter):
+            # Dynamiczna i nieliniowa aktualizacja parametrów wg modyfikacji A
+            w = 0.9 - 0.5 * (iter / self.max_iter)**2 # paraboliczny spadek bezwładności
+            c1 = 2.5 - (iter / self.max_iter) * (2.5 - 0.5) # poznawczy - niezależność na początku (c1=2.5)
+            c2 = 0.5 + (iter / self.max_iter) * (2.5 - 0.5) # przyciąganie - gwałtowne zbieganie na końcu (c2=2.5)
+            
+            for i in range(self.ps):
+                # --- WARSTWA GENETYCZNA (Egzemplarze) ---
+
+                # Krzyżowanie w topologii pierścieniowej
+                n_i1 = i - 1 if i > 0 else self.ps - 1
+                n_i2 = i + 1 if i < self.ps - 1 else 0
+                
+                O_i = np.zeros(self.dim)
+                for d in range(self.dim):
+                    k = np.random.randint(0, self.ps)
+                    if self.pbest_fit[i] < self.pbest_fit[k]:
+                        # Uczenie od sąsiadów z pierścienia
+                        r_d = np.random.rand()
+                        O_i[d] = r_d * self.pbest[n_i1, d] + (1 - r_d) * self.pbest[n_i2, d]
+                    else:
+                        # Uczenie od losowego Pbest
+                        O_i[d] = self.pbest[k, d]
+                
+                # Mutacja z rozkładem Cauchy'ego (Modyfikacja B)
+                for d in range(self.dim):
+                    if np.random.rand() < self.pm:
+                        # Modyfikujemy obecną pozycję egzemplarza (drobne korekty + rzadkie skoki)
+                        O_i[d] += np.random.standard_cauchy()
+                        # Ograniczamy do przestrzeni poszukiwań
+                        O_i[d] = np.clip(O_i[d], self.lb, self.ub)
+                
+                # Selekcja egzemplarza 
+                o_fit = self.obj_func(O_i)
+                if o_fit < self.e_fit[i]:
+                    self.E[i] = O_i
+                    self.e_fit[i] = o_fit
+                    self.stagnation_counter[i] = 0
+                    if o_fit < self.gbest_fit:
+                        self.gbest_fit = o_fit
+                        self.gbest = np.copy(O_i)
+                else:
+                    self.stagnation_counter[i] += 1
+                
+                # Re-selekcja przy stagnacji (Tournament selection) 
+                if self.stagnation_counter[i] >= self.sz:
+                    participants = np.random.choice(self.ps, int(0.2 * self.ps), replace=False)
+                    best_participant = participants[np.argmin(self.pbest_fit[participants])]
+                    self.E[i] = np.copy(self.pbest[best_participant])
+                    self.e_fit[i] = self.pbest_fit[best_participant]
+                    self.stagnation_counter[i] = 0
+
+                # --- WARSTWA PSO (Aktualizacja cząstki) ---
+                # Równanie prędkości 
+                r1, r2 = np.random.rand(self.dim), np.random.rand(self.dim)
+                self.V[i] = (
+                        w * self.V[i]
+                        + c1 * r1 * (self.E[i] - self.X[i])
+                        + c2 * r2 * (self.gbest - self.X[i])
+                        )
+                
+                # Aktualizacja pozycji 
+                self.X[i] = self.X[i] + self.V[i]
+                self.X[i] = np.clip(self.X[i], self.lb, self.ub) # Granice
+                
+                # Ocena i aktualizacja Pbest oraz Gbest
+                current_fit = self.obj_func(self.X[i])
+                if current_fit < self.pbest_fit[i]:
+                    self.pbest_fit[i] = current_fit
+                    self.pbest[i] = np.copy(self.X[i])
+                    
+                    if current_fit < self.gbest_fit:
+                        self.gbest_fit = current_fit
+                        self.gbest = np.copy(self.X[i])
+            
+            if (iter + 1) % 100 == 0:
+                print(f"Iteracja {iter+1}/{self.max_iter}, Najlepszy wynik: {self.gbest_fit:.5e}")
+                
+        return self.gbest, self.gbest_fit
